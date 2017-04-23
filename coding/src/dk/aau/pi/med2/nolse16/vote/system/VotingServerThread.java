e340070f6339504fdfd1f0553a0a4984746c6b74
Unknown <nolse16@student.aau.dk>
2017-04-23 15:29:29 +0200

    Changes

--- coding/src/dk/aau/pi/med2/nolse16/vote/system/VotingServerThread.java
+++ coding/src/dk/aau/pi/med2/nolse16/vote/system/VotingServerThread.java
@@ -12,14 +12,13 @@ public class VotingServerThread extends Thread {
 	}
 
 	public void run() {
-
 		try {
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 			String inputLine, outputLine;
-			KnockKnockProtocol kkp = new KnockKnockProtocol();
-			outputLine = kkp.processInput(null);
+//			KnockKnockProtocol kkp = new KnockKnockProtocol();
+//			outputLine = kkp.processInput(null);
 			out.println(outputLine);
 
 			while ((inputLine = in.readLine()) != null) {
