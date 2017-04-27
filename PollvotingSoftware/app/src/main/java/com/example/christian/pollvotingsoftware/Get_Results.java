931bcd2054ab8e99e1ce8bd6a256edc0c713c79f
Unknown <sscsd@live.dk>
2017-04-27 20:23:07 +0200

    Get Results

--- PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Get_Results.java
+++ PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Get_Results.java
@@ -2,17 +2,24 @@ package com.example.christian.pollvotingsoftware;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by Christian on 27-04-2017.
  */
 
 public class Get_Results extends AppCompatActivity {
+    String temp = "0,0,Yes or no?,Yes,No";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_create_poll);
+        setContentView(R.layout.content_get_results);
         {
+            String[] parts = temp.split(",");
+            for (int i=0; i<parts.length; i++){
+                Log.d("hansi", parts[i]);
+
+        }
 
         }
 
