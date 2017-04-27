eea2218f552843d71f23b5bb958a9c75a4ff22e0
Unknown <nolse16@student.aau.dk>
2017-04-27 20:22:06 +0200

    TEST

--- PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Poll_Overview.java
+++ PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Poll_Overview.java
@@ -1,63 +1,1 @@
-package com.example.christian.pollvotingsoftware;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-
-/**
- * Created by Christian on 27-04-2017.
- */
-
-public class Poll_Overview extends AppCompatActivity {
-
-    public Button createpoll;
-
-    public Button getresults;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.cont_polls_overview);
-
-        createpoll = (Button)  findViewById(R.id.createpoll);
-        getresults = (Button) findViewById(R.id.getresults);
-
-        init();
-    }
-
-    public void init() {
-        TableLayout ll = (TableLayout) findViewById(R.id.displayLinear);
-        for (int i = 0; i < 20; i++) {
-            if (i % 2 == 0) {
-                TableRow desc = new TableRow(this);
-                TableRow.LayoutParams pl = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-                desc.setLayoutParams(pl);
-                TextView description = new TextView(this);
-                description.setText("NEW TESTING");
-                desc.addView(description);
-                ll.addView(desc, i);
-            } else {
-                TableRow row = new TableRow(this);
-                TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-                row.setLayoutParams(lp);
-                Button button = new Button(this);
-                Button button2 = new Button(this);
-                row.addView(button);
-                row.addView(button2);
-                ll.addView(row, i);
-            }
-        }
-    }
-    public void createPoll(View view) {
-        Intent g = new Intent(Poll_Overview.this, Create_Poll.class);
-        startActivity(g);
-    }
-    public void getResults(View view) {
-        Intent g = new Intent(Poll_Overview.this, Get_Results.class);
-        startActivity(g);
-    }
-}
+package com.example.christian.pollvotingsoftware;import android.content.Intent;import android.os.Bundle;import android.support.v7.app.AppCompatActivity;import android.view.View;import android.widget.Button;import android.widget.TableLayout;import android.widget.TableRow;import android.widget.TextView;import java.io.BufferedReader;import java.io.IOException;import java.io.InputStreamReader;/** * Created by Christian on 27-04-2017. */public class Poll_Overview extends AppCompatActivity {    public Button createpoll;    public Button getresults;    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.cont_polls_overview);        createpoll = (Button) findViewById(R.id.createpoll);        getresults = (Button) findViewById(R.id.getresults);        init();    }    public void init() {//        loadFromServer();        TableLayout ll = (TableLayout) findViewById(R.id.displayLinear);        for (int i = 0; i < 20; i++) {            if (i % 2 == 0) {                TableRow desc = new TableRow(this);                TableRow.LayoutParams pl = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);                desc.setLayoutParams(pl);                TextView description = new TextView(this);                description.setText("NEW TESTING");                desc.addView(description);                ll.addView(desc, i);            } else {                TableRow row = new TableRow(this);                TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);                row.setLayoutParams(lp);                Button button = new Button(this);                Button button2 = new Button(this);                row.addView(button);                row.addView(button2);                ll.addView(row, i);            }        }    }//    public String[] loadFromServer() {//        String modifiedSentence;//        try {//            Main_Menu.outToServer.writeBytes("loadPolls" + '\n');//            //waits and read input from, maybe not necessary in all cases//            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//            //modified sentence == results from server//            modifiedSentence = inFromServer.readLine();//            System.out.println(modifiedSentence);//        } catch (IOException e) {//            e.printStackTrace();//        }////        if (modifiedSentence != null) {////            System.out.println(modifiedSentence);////        }////        System.out.println("reaches here");//return null;//    }    public void createPoll(View view) {        Intent g = new Intent(Poll_Overview.this, Create_Poll.class);        startActivity(g);    }    public void getResults(View view) {        Intent g = new Intent(Poll_Overview.this, Get_Results.class);        startActivity(g);    }}
