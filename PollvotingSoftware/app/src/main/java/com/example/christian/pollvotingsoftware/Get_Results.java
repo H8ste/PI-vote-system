9d9bd489c3e1f07c01b69cbfde3cd3e5e58a7dff
Unknown <sscsd@live.dk>
2017-04-27 16:41:35 +0200

    It looks good son

---
+++ PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Get_Results.java
@@ -0,0 +1,20 @@
+package com.example.christian.pollvotingsoftware;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Christian on 27-04-2017.
+ */
+
+public class Get_Results extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_create_poll);
+        {
+
+        }
+
+    }
+}
